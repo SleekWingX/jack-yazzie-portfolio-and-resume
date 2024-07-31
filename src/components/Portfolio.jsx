fe8e77049c72f89266e15194270a974b89cbd6a5
SleekWingX <alexisyazzie365@gmail.com>
2024-07-30 21:47:11 -0600

    added some styling and ensured images show

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,7 +1,7 @@
 import Project from './Project';
 
 const projects = [
-  { title: 'Progressive Web Application - Jate', img: '../assets/img/jate.png', live: 'https://progressive-web-application-jate.onrender.com/', repo: 'https://github.com/SleekWingX/progressive-web-application' },
+  { title: 'Progressive Web Application - Jate', img: 'src/assets/img/jate.png', live: 'https://progressive-web-application-jate.onrender.com/', repo: 'https://github.com/SleekWingX/progressive-web-application' },
   { title: 'Project 2', img: 'path/to/image2.jpg', live: 'https://live2.com', repo: 'https://github.com/repo2' },
   // Add more projects as needed
 ];
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -2,8 +2,8 @@ import PropTypes from 'prop-types';
 
 const Project = ({ title, img, live, repo }) => (
   <div className="project">
-    <img src={img} alt={title} />
     <h3>{title}</h3>
+    <img src={img} alt={title} />
     <a href={live} target="_blank" rel="noopener noreferrer">Live Site</a>
     <a href={repo} target="_blank" rel="noopener noreferrer">GitHub Repo</a>
   </div>
