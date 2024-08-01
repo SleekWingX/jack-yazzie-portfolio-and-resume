1b9b14c656cebdfda0e5641cb594e451b6830b41
SleekWingX <alexisyazzie365@gmail.com>
2024-07-31 20:04:21 -0600

    added portfolio links and images

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,9 +1,12 @@
 import Project from './Project';
 
 const projects = [
+  { title: 'Note Taker', img: 'src/assets/img/NoteTaker.JPG', live: 'https://note-list-page.onrender.com/', repo: 'https://github.com/SleekWingX/note-list-page' },
+  { title: 'Nest Cast', img: 'src/assets/img/NestCast.JPG', live: 'https://mpredelus3.github.io/nest-cast/home.html', repo: 'https://github.com/mpredelus3/nest-cast'},
   { title: 'Progressive Web Application - Jate', img: 'src/assets/img/jate.png', live: 'https://progressive-web-application-jate.onrender.com/', repo: 'https://github.com/SleekWingX/progressive-web-application' },
-  { title: 'Project 2', img: 'path/to/image2.jpg', live: 'https://live2.com', repo: 'https://github.com/repo2' },
-  // Add more projects as needed
+  { title: 'Task Board', img: 'src/assets/img/TaskBoard.JPG', live: 'https://sleekwingx.github.io/week-5-challenge-task-board/', repo: 'https://github.com/SleekWingX/week-5-challenge-task-board' },
+  { title: 'Weather Dashboard', img: 'src/assets/img/WeatherDashboard.JPG', live: 'https://sleekwingx.github.io/week-6-weather-dashboard/', repo: 'https://github.com/SleekWingX/week-6-weather-dashboard' },
+  { title: 'Employee Payroll Tracker', img: 'src/assets/img/EmployeePayrollTracker.jpg', live: 'https://sleekwingx.github.io/week-3-javascript-project-jack-yazzie/', repo: 'https://github.com/SleekWingX/week-3-javascript-project-jack-yazzie' }
 ];
 
 const Portfolio = () => (
